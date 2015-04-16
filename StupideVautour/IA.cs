803d5e58906c537cd6971b83a3bf28bc3a10954a
jonathan.platteau@yahoo.fr <jonathan.platteau@yahoo.fr>
2015-04-16 18:00:47 +0200

    lol

--- StupideVautour/IA.cs
+++ StupideVautour/IA.cs
@@ -3,14 +3,17 @@
 public class IA
 {
     int num;
+    int nbJoueur;
 	
-    public IA(int num)
+    public IA(int num, int nbJoueur)
 	{
         this.num = num;
+        this.nbJoueur = nbJoueur;
 	}
 
     public int Joue(int[,] mains_joueurs, int point)
     {
+        
         return 1;
     }
 }
---
+++ StupideVautour/IAnormal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StupideVautour
+{
+    class IAnormal : IA
+    {
+        public int Joue()
+        {
+
+            return 0;
+        }
+    }
+}
